7a300433860709280116d425687f909095b20a71
mxnf12 <130280770+mxnf12@users.noreply.github.com>
2023-05-20 20:37:07 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,9 +119,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>您的专属AI神器</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          您的专属AI神器
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
