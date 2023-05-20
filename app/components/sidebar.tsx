04144f8f972f0d530823157f90c03b954b8e7bd9
mxnf12 <130280770+mxnf12@users.noreply.github.com>
2023-05-20 20:50:04 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,7 +119,7 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>您的专属AI神器</div>
+        <div className={styles["sidebar-title"]}>GPT Pro</div>
         <div className={styles["sidebar-sub-title"]}>
           您的专属AI神器
         </div>
